68d738cdc9c27f59a48e8a0206ce5b2da0ac6946
ventisep <paul.ventisei@tesco.net>
2015-06-16 23:05:44 +0100

    Load Copy of App Engine Files
    Initial Copy of App Engine Files that have been taken from Rowing
    Network

---
+++ Rowing Head and Regatta App/secrets.py
@@ -0,0 +1,53 @@
+# Copy this file into secrets.py and set keys, secrets and scopes.
+
+# This is a session secret key used by webapp2 framework.
+# Get 'a random and long string' from here: 
+# http://clsc.net/tools/random-string-generator.php
+# or execute this from a python shell: import os; os.urandom(64)
+SESSION_KEY = '41f163w4f2oi8c8qqxx782815481au46cmgpluww61880ve3b8'
+# Google APIs
+GOOGLE_APP_ID = ''
+GOOGLE_APP_SECRET = ''
+
+# Facebook auth apis
+FACEBOOK_APP_ID = '262766713892199'
+FACEBOOK_APP_SECRET = '9c394a8a339110d7681e1d6d4cea65cb'
+
+# Key/secret for both LinkedIn OAuth 1.0a and OAuth 2.0
+# https://www.linkedin.com/secure/developer
+LINKEDIN_KEY = 'consumer key'
+LINKEDIN_SECRET = 'consumer secret'
+
+# https://manage.dev.live.com/AddApplication.aspx
+# https://manage.dev.live.com/Applications/Index
+WL_CLIENT_ID = 'client id'
+WL_CLIENT_SECRET = 'client secret'
+
+# https://dev.twitter.com/apps
+TWITTER_CONSUMER_KEY = 'oauth1.0a consumer key'
+TWITTER_CONSUMER_SECRET = 'oauth1.0a consumer secret'
+
+# https://foursquare.com/developers/apps
+FOURSQUARE_CLIENT_ID = 'client id'
+FOURSQUARE_CLIENT_SECRET = 'client secret'
+
+# config that summarizes the above
+AUTH_CONFIG = {
+  # OAuth 2.0 providers
+  'google'      : (GOOGLE_APP_ID, GOOGLE_APP_SECRET,
+                  'https://www.googleapis.com/auth/userinfo.profile'),
+  'linkedin2'   : (LINKEDIN_KEY, LINKEDIN_SECRET,
+                  'r_basicprofile'),
+  'facebook'    : (FACEBOOK_APP_ID, FACEBOOK_APP_SECRET,
+                  'user_about_me'),
+  'windows_live': (WL_CLIENT_ID, WL_CLIENT_SECRET,
+                  'wl.signin'),
+  'foursquare'  : (FOURSQUARE_CLIENT_ID,FOURSQUARE_CLIENT_SECRET,
+                  'authorization_code'),
+
+  # OAuth 1.0 providers don't have scopes
+  'twitter'     : (TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET),
+  'linkedin'    : (LINKEDIN_KEY, LINKEDIN_SECRET),
+
+  # OpenID doesn't need any key/secret
+}
